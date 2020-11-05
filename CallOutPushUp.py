62c13477896bd5381a1393a547949977ae171ef1
Mingzhe Zou <soapclancy@hotmail.com>
2020-11-05 22:23:15 +0000

    replace gTTs and pygame with pyttsx3

--- CallOutPushUp.py
+++ CallOutPushUp.py
@@ -1,10 +1,6 @@
-import copy
+from typing import List, Union
+import pyttsx3
 import time
-from io import BytesIO
-from typing import Iterable, Union
-
-import pygame
-from gtts import gTTS
 
 """
 This is a simple implementation of text-to-speech (TTS) assistant, which helps time push up workouts.
@@ -16,55 +12,19 @@ Total number of circles of push up: 'total_circles'
 """
 
 
-def call_out(text: Iterable[str], repeat_times: int,
+def call_out(text: List[str], repeat_times: int,
              minimum_seconds_per_call_out: Union[int, float] = 1):
-    tts_obj_list_one = [gTTS(text=this_text) for this_text in text]
-    tts_obj_list_rank = [gTTS(text=f"第{i}个", lang='zh-cn') for i in range(1, repeat_times + 1)]
-
-    bytes_io_obj = []
-    rank_of_circle = {}
-    for this_tts_obj in tts_obj_list_one:
-        # In one circle
-        this_bytes_io_obj = BytesIO()
-        this_tts_obj.write_to_fp(this_bytes_io_obj)
-        this_bytes_io_obj.seek(0)
-        bytes_io_obj.append(this_bytes_io_obj)
-    bytes_io_obj = tuple(bytes_io_obj * repeat_times)  # shallow copy to repeat bytes_io_obj for repeat_times
-
-    for i, this_tts_obj in enumerate(tts_obj_list_rank):
-        # Circle rank
-        this_bytes_io_obj = BytesIO()
-        this_tts_obj.write_to_fp(this_bytes_io_obj)
-        this_bytes_io_obj.seek(0)
-        rank_of_circle.setdefault(int((i + 1) * tts_obj_list_one.__len__()), this_bytes_io_obj)
-
-    pygame.mixer.init()
-    time_start = time.time()
-    for i in range(bytes_io_obj.__len__() + 1):
-        if i in rank_of_circle:
-            pygame.mixer.music.load(rank_of_circle[i])
-            pygame.mixer.music.play()
-            while True:
-                if not pygame.mixer.music.get_busy():
-                    break
-        if i == bytes_io_obj.__len__():
-            break
-
-        pygame.mixer.music.load(copy.deepcopy(bytes_io_obj[i]))
-        pygame.mixer.music.play()
-        while time.time() - time_start < minimum_seconds_per_call_out:
-            continue
-        else:
-            time_start = time.time()
-        while True:
-            if not pygame.mixer.music.get_busy():
-                break
-    pygame.quit()
+    time.sleep(5)
+    engine = pyttsx3.init()
+    engine.setProperty('rate', int(minimum_seconds_per_call_out * 100))  # setting up new voice rate
+    for i in range(repeat_times):
+        engine.say(str(text + [f"第{i + 1}个"]))
+        engine.runAndWait()
 
 
-def do_push_up(up_duration_in_one_push_up_in_seconds: int = 4,
+def do_push_up(up_duration_in_one_push_up_in_seconds: int = 3,
                hold_on_duration_in_one_push_up_in_seconds: int = 1,
-               down_duration_in_one_push_up_in_seconds: int = 5,
+               down_duration_in_one_push_up_in_seconds: int = 3,
                total_circles: int = 50):
     one_circle = [str(i) for i in range(1, up_duration_in_one_push_up_in_seconds + 1)] + \
                  ['hold'] * hold_on_duration_in_one_push_up_in_seconds + \
